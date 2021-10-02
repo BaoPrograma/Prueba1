6faca551ab92d50c89997c0aee6483329a1f4517
BaoPrograma <91781414+BaoPrograma@users.noreply.github.com>
2021-10-02 18:25:51 +0200

    Version1

---
+++ Schedule/Schedule/Auxiliar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedule
+{
+    public enum TipoFranja
+    {
+        Once = 0,
+        Recurring = 1
+    }
+
+    public enum TipoPeriodicidad
+    {
+        Daily = 0
+    }
+}
---
+++ Schedule/Schedule/Configuracion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedule
+{
+    public class Configuracion
+    {
+        public Configuracion()
+        {
+        }
+
+        public TipoFranja Tipo;
+        public DateTime? FechaPaso;
+        public TipoPeriodicidad? Periodicidad;
+        public int? DiasPeriodicidad;
+        public DateTime FechaInicio;
+        public DateTime? FechaFin;
+        public bool Enabled;
+    }
+}
