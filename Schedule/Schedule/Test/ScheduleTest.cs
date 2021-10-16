1733a967652b414c3f7108b0bfc19658c42dafec
BaoPrograma <91781414+BaoPrograma@users.noreply.github.com>
2021-10-16 13:54:58 +0200

    Version 4

--- Schedule/Schedule/Test/ScheduleTest.cs
+++ Schedule/Schedule/Test/ScheduleTest.cs
@@ -15,7 +15,6 @@ namespace Schedule
         public ScheduleTest()
         {
             this.configuracion = new Configuracion();
-            this.procesador = new Schedule(this.configuracion);
         }
 
 
@@ -29,39 +28,135 @@ namespace Schedule
             this.configuracion.FechaPaso = new DateTime(2021, 8, 1).AddHours(14);
             this.configuracion.Periodicidad = TipoPeriodicidad.Daily;
             DateTime LaFecha = new DateTime(2021 , 1 , 4);
+            this.procesador = new Schedule(this.configuracion);
 
             //Act
-            Output[] LaSalidas = this.procesador.Procesar(LaFecha);
+            Output[] LaSalidas = this.procesador.ProcesarPeriodo(LaFecha);
 
             //Assert
             Assert.Equal(this.configuracion.FechaPaso, LaSalidas[0].FechaSalida);
         }
 
         [Fact]
-        public void Lanzar_recurring_diario_DiasPeriodicidad_debe_ser_mayor_o_igual_a_0()
+        public void Lanzar_recurring_WeeklyPaso_debe_ser_mayor_o_igual_a_0()
         {
             // Arrange
             this.configuracion.Enabled = true;
             this.configuracion.FechaInicio = new DateTime(2021, 1, 1);
             this.configuracion.Tipo = TipoFranja.Recurring;
-            this.configuracion.Periodicidad = TipoPeriodicidad.Daily;
-            this.configuracion.DiasPeriodicidad = 0;
+            this.configuracion.Periodicidad = TipoPeriodicidad.Weekly;
+            this.configuracion.WeeklyPaso = 1;
             DateTime LaFecha = new DateTime(2021, 1, 4);
+            this.procesador = new Schedule(this.configuracion);
 
             //Act
-            Output[] LaSalidas = this.procesador.Procesar(LaFecha);
+            Output[] LaSalidas = this.procesador.ProcesarPeriodo(LaFecha);
 
             //Assert
-            if (LaSalidas.Length == 0)
-            {
-                Assert.Equal(LaFecha, LaFecha);
-            }
-            else
-            {
-                Assert.Equal(LaFecha.AddDays(this.configuracion.DiasPeriodicidad), LaSalidas[LaSalidas.Length- 1].FechaSalida);
-            }
+            Assert.True(this.configuracion.WeeklyPaso >= 0);
         }
 
+        [Fact]
+        public void Lanzar_recurring_WeeklyPaso_no_debe_ser_menor_0()
+        {
+            // Arrange
+            this.configuracion.Enabled = true;
+            this.configuracion.FechaInicio = new DateTime(2021, 1, 1);
+            this.configuracion.Tipo = TipoFranja.Recurring;
+            this.configuracion.Periodicidad = TipoPeriodicidad.Weekly;
+            this.configuracion.WeeklyPaso = -1;
+            DateTime LaFecha = new DateTime(2021, 1, 4);
+            this.procesador = new Schedule(this.configuracion);
 
+            //Act
+            Output[] LaSalidas = this.procesador.ProcesarPeriodo(LaFecha);
+
+            //Assert
+            Assert.False(this.configuracion.WeeklyPaso < 0);
+        }
+
+        [Fact]
+        public void Lanzar_once_solo_ocurre_Diary()
+        {
+            // Arrange
+            this.configuracion.Enabled = true;
+            this.configuracion.FechaInicio = new DateTime(2021, 1, 1);
+            this.configuracion.Tipo = TipoFranja.Once;
+            this.configuracion.FechaPaso = new DateTime(2021, 8, 1).AddHours(14);
+            this.configuracion.Periodicidad = TipoPeriodicidad.Daily;
+            DateTime LaFecha = new DateTime(2021, 1, 4);
+            this.procesador = new Schedule(this.configuracion);
+
+            //Act
+            Output[] LaSalidas = this.procesador.ProcesarPeriodo(LaFecha);
+
+            //Assert
+            Assert.Equal(this.configuracion.FechaPaso, LaSalidas[0].FechaSalida);
+        }
+
+        [Fact]
+        public void Lanzar_once_no_ocurre_Weekly()
+        {
+            // Arrange
+            this.configuracion.Enabled = true;
+            this.configuracion.FechaInicio = new DateTime(2021, 1, 1);
+            this.configuracion.Tipo = TipoFranja.Once;
+            this.configuracion.FechaPaso = new DateTime(2021, 8, 1).AddHours(14);
+            this.configuracion.Periodicidad = TipoPeriodicidad.Weekly;
+            DateTime LaFecha = new DateTime(2021, 1, 4);
+            this.procesador = new Schedule(this.configuracion);
+
+            //Act
+            Output[] LaSalidas = this.procesador.ProcesarPeriodo(LaFecha);
+
+            //Assert
+            Assert.NotEqual(TipoPeriodicidad.Daily, this.configuracion.Periodicidad);
+        }
+
+        [Fact]
+        public void Lanzar_recurring_HourPaso_debe_ser_mayor_0()
+        {
+            // Arrange
+            this.configuracion.Enabled = true;
+            this.configuracion.FechaInicio = new DateTime(2021, 1, 1);
+            this.configuracion.Tipo = TipoFranja.Recurring;
+            this.configuracion.Periodicidad = TipoPeriodicidad.Weekly;
+            this.configuracion.WeeklyPaso = -1;
+            this.configuracion.HourPaso = 1;
+            DateTime LaFecha = new DateTime(2021, 1, 4);
+            this.procesador = new Schedule(this.configuracion);
+
+            //Act
+            Output[] LaSalidas = this.procesador.ProcesarPeriodo(LaFecha);
+
+            //Assert
+            Assert.True(this.configuracion.HourPaso > 0);
+        }
+
+        [Fact]
+        public void Lanzar_recurring_Weekly()
+        { 
+            // Arrange
+            this.configuracion.Enabled = true;
+            this.configuracion.FechaInicio = new DateTime(2020, 1, 1);
+            this.configuracion.FechaFin = new DateTime(2020, 1, 27);
+            this.configuracion.Tipo = TipoFranja.Recurring;
+            this.configuracion.Periodicidad = TipoPeriodicidad.Weekly;
+            this.configuracion.WeeklyPaso = 2;
+            this.configuracion.HourPaso = 2;
+            this.configuracion.HoraDesde = new DateTime(1900, 1, 1, 8, 0, 0);
+            this.configuracion.HoraHasta = new DateTime(1900, 1, 1, 12, 0, 0);
+            this.configuracion.WeekMonday = true;
+            this.configuracion.WeekThursday = true;
+            this.configuracion.WeekFriday = true;
+            DateTime LaFecha = new DateTime(2020, 1, 1);
+            this.procesador = new Schedule(this.configuracion);
+
+            //Act
+            Output[] LaSalidas = this.procesador.ProcesarPeriodo(LaFecha);
+
+            //Assert
+            Assert.True(LaSalidas.Count() > 0);
+        }
     }
 }
