2494ff95a475441964d49e063efcfe387b296bea
BaoPrograma <91781414+BaoPrograma@users.noreply.github.com>
2022-01-23 11:31:15 +0100

    Version 18

---
+++ Schedule/Model/Helper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScheduleModel
+{
+    public enum TypeStep
+    {
+        Once = 0,
+        Recurring = 1
+    }
+
+    public enum TypeTimeStep
+    {
+        Daily = 0,
+        Weekly = 1,
+        Monthly = 2
+    }
+
+    public enum TypeWeekStep
+    {
+        First = 1,
+        Second = 2,
+        Third = 3,
+        Fourth = 4,
+        Last = 5
+    }
+    public enum TypeDayWeekStep
+    {
+        Monday = 0,
+        Tuesday = 1,
+        Wednesday = 2,
+        Thursday = 3,
+        Friday = 4,
+        Saturday = 5,
+        Sunday = 6,
+        Day = 7,
+        WeekDay = 8,
+        WeekendDay = 9
+    }
+
+    public enum DailyFrequency
+    {
+        Once,
+        Every
+    }
+
+    public enum Languages
+    {
+        en_GB = 0,
+        es_ES = 1,
+        en_US = 2
+    }
+}
---
+++ Schedule/Model/Model.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ScheduleModel
+{
+    public class ScheduleContext : DbContext
+    {
+        public DbSet<ScheduleConfiguration> Configuration { get; set; }
+
+        public ScheduleContext(DbContextOptions<ScheduleContext> options)
+        : base(options)
+        {
+        }
+    }
+
+    public class ScheduleConfiguration
+    {
+        [Key]
+        public int SchedulerId { get; set; }
+
+        public TypeStep TimeType { get; set; }
+        public DateTime? DateStep { get; set; }
+        public DateTime? DateFrom { get; set; }
+        public DateTime? DateTo { get; set; }
+
+        public TypeTimeStep? TypeRecurring { get; set; }
+
+        #region recurring
+        public int? HourStep { get; set; }
+        public bool Enabled { get; set; }
+        public DateTime? HourFrom { get; set; }
+        public DateTime? HourTo { get; set; }
+
+        public int DailyStep { get; set; }
+
+        public int WeekStep { get; set; }
+        public bool WeeklyMonday { get; set; }
+        public bool WeeklyTuesday { get; set; }
+        public bool WeeklyWednesday { get; set; }
+        public bool WeeklyThursday { get; set; }
+        public bool WeeklyFriday { get; set; }
+        public bool WeeklySaturday { get; set; }
+        public bool WeeklySunday { get; set; }
+
+        public bool? MonthlyOnce { get; set; }
+        public int? MonthlyOnceDay { get; set; }
+        public int? MonthlyOnceMonthSteps { get; set; }
+        public DailyFrequency? TypeDailyFrequency { get; set; }
+
+        public bool? MonthlyMore { get; set; }
+        public TypeWeekStep? MonthlyMoreWeekStep { get; set; }
+        public TypeDayWeekStep? MonthlyMoreOrderDayWeekStep { get; set; }
+        public int? MonthlyMoreMonthSteps { get; set; }
+
+        public Languages Language { get; set; }
+
+        #endregion
+    }
+}
